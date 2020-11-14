03486155f9e37bc4485d6655a1bc1c637ab4806a
valentinogagliardi <valentino@valentinog.com>
2020-11-14 18:21:38 +0100

    add setup.py

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(
+    name="tiny_python_utils",
+    version="0.0.1",
+    description="Personal Python utils",
+    author="Valentino Gagliardi",
+    packages=["tiny_python_utils"],
+)
